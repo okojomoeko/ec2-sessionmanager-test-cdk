c7e115b44e350405672bed2e8698b694514ef5a5
okojomoeko <chikoro.size@gmail.com>
2021-11-11 23:43:38 +0900

    commit

--- README.md
+++ README.md
@@ -1,3 +1,47 @@
+# Overview 
+
+Private Subnetの適当なEC2 Instanceから、Session Managerを使ったSSH portforwardingでPrivate Subnetに置いたWeb Server用EC2 Instanceに接続する。
+
+## 使い方
+
+deployしたらまずはhoge instanceにアクセスできるか確認する。
+
+keypairがないのでsshできないが、送りつける([参考](https://qiita.com/dairappa/items/96d55e929cce22ba90a5))
+
+```
+aws ec2-instance-connect send-ssh-public-key \
+    --instance-id YOURINSTANCEID \
+    --availability-zone us-east-1a \
+    --instance-os-user ec2-user \
+    --ssh-public-key file://$HOME/.ssh/id_rsa.pub
+```
+
+cdkで作ってしまってもいいかもしれない([参考](https://github.com/udondan/cdk-ec2-key-pair))
+
+ssh config
+```
+host i-* mi-*
+    IdentityFile "~/.ssh/id_rsa"
+    ProxyCommand sh -c "aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p'"
+```
+
+とりあえず上記の設定をして、instance1にsshできる
+
+instance2にnginxをinstallして建てる([参考](https://qiita.com/Hide-Zaemon/items/f4a0599b7c8cb3559ca0))
+そのうちcloud-initする
+```
+sudo yum update
+sudo amazon-linux-extras enable nginx1
+sudo yum -y install nginx
+sudo systemctl start nginx.service
+```
+
+そして、instance2のinboundはtcp(80)がallowとなっているので、session managerでportfordingする
+
+`ssh ec2-user@YOURINSTANCEID -fNL 8080:PRIVAEADDRESS:80`
+
+localhost:8080でアクセスできたら成功
+
 # Welcome to your CDK TypeScript project!
 
 You should explore the contents of this project. It demonstrates a CDK app with an instance of a stack (`TestCdkStack`)
--- lib/test-cdk-stack.ts
+++ lib/test-cdk-stack.ts
@@ -1,18 +1,67 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as iam from '@aws-cdk/aws-iam';
+import { readFileSync } from 'fs';
 
 export class TestCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'TestCdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    // Create new VPC with 2 Subnets
+    const vpc = new ec2.Vpc(this, 'NewsBlogVPC', {
+      maxAzs : 1
     });
 
-    const topic = new sns.Topic(this, 'TestCdkTopic');
+    // create sg
+    const securityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
+      vpc,
+      description: 'For SSM only',
+      allowAllOutbound: true
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // create sg2
+    const securityGroup2 = new ec2.SecurityGroup(this, 'SecurityGroup2', {
+      vpc,
+      description: 'For Tunnel Only',
+      allowAllOutbound: true,
+    });
+
+    // Open port for http
+    securityGroup2.connections.allowFrom(securityGroup, ec2.Port.tcp(80), 'for http server');
+
+    const role = new iam.Role(this, 'ec2Role', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
+    })
+
+    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
+
+    // Use Latest Amazon Linux Image - CPU Type X86_64
+    const ami = new ec2.AmazonLinuxImage({
+      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      cpuType: ec2.AmazonLinuxCpuType.X86_64
+    });
+
+
+    // // If you require cloud-init
+    // const userDataFile = readFileSync('./lib/user-data.yml', 'utf8');
+
+    // Create the instance using the Security Group, AMI, and KeyPair defined in the VPC created
+    const ec2Instance = new ec2.Instance(this, 'Instance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ami,
+      securityGroup: securityGroup,
+      role: role,
+    });
+
+    const ec2Instance2 = new ec2.Instance(this, 'Instance2', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ami,
+      securityGroup: securityGroup2,
+      role: role,
+    });
+    // ec2Instance2.addUserData(userDataFile);
+
+
   }
 }
